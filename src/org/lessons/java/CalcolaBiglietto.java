1a8d8300a816abda5c44af677000fda71e1c40d8
SimoneVilardo <simovila95@gmail.com>
2023-11-09 17:44:35 +0100

    esercizio finito

---
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -0,0 +1,37 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Quanti chilometri devi percorrere?");
+		String strKm = in.nextLine();
+		int intKm = Integer.valueOf(strKm);
+		
+		
+		System.out.print("Quanti anni hai?");
+		String strEta = in.nextLine();
+		int intEta = Integer.valueOf(strEta);
+		
+		in.close();
+		
+		double price = intKm * 0.21;
+		
+		double newPrice = 0;
+		
+		if (intEta < 18) {
+			newPrice = price * 0.8;
+		}else if (intEta > 65) {
+			newPrice = price * 0.6;
+		}else {
+			newPrice = price;
+		}
+		
+		System.out.println("Il prezzo del tuo biglietto è di:" + newPrice + "€");
+	}
+
+}
